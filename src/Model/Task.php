641f5a17ca42ded7fb9b8d87ba010f2982a50a00
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-12-10 22:29:35 -0500

    task_model

--- src/Model/Task.php
+++ src/Model/Task.php
@@ -9,7 +9,23 @@ class Task extends  Database{
        $stmt = $this->connect()->query($sql);
        return $stmt->fetchAll();
     }
+
+    public function add($title, $contenue){
+      $sql = "INSERT INTO tache (nom, contenue) VALUES (?, ?)" ;
+      $stmt = $this->connect()->prepare($sql);
+      $stmt->execute([$title, $contenue]);
+    }
+
+    public function complete($id){
+        $sql = "UPDATE tache SET statut = 1 WHERE id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id]);
+    }
     
-    
+    public function delete($id){
+        $sql = "DELETE FROM tache WHERE id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id]);
+    }
     
 }
